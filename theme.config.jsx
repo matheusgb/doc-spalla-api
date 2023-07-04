aca7c3155a07d7e106b246f068e07108f24eeb4d
matheusgb <matheusgbr98@gmail.com>
2023-07-04 15:24:46 -0300

    initial commit

---
+++ theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+  logo: <img 
+  src="https://res.cloudinary.com/postman/image/upload/t_team_logo/v1635649856/team/88326d517986c17ae791cb5eae678761bc5f04f77198730c0d6fad9ee5ba9a6f"
+  alt="Spalla Logo"
+  height={45}
+  width={45}/>
+}
