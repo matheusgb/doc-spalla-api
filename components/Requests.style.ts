20971fd048656536f97b9d1a40a28b347ddbe333
matheusgb <matheusgbr98@gmail.com>
2023-07-06 00:41:45 -0300

    feat: improves endpoints component

--- components/Requests.style.ts
+++ components/Requests.style.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { type IMethodsColors, type IRequests } from '../interfaces'
+import { Pre } from 'nextra/components'
 
 const methods: IMethodsColors = {
   GET: '#5f3be2',
@@ -9,6 +10,41 @@ const methods: IMethodsColors = {
   DELETE: '#ff0000'
 }
 
-export const Method = styled.p<IRequests>`
-  color: ${(props) => methods[props.method]};
-  `
+export const Container = styled.section`
+  margin-top: 10px;
+  display: flex;
+  justify-content: center;
+  height: 30vh;
+`
+export const DivContainer = styled.div`
+  width: 100%;
+`
+
+export const DivContainerHead = styled.div`
+  display: flex;
+  align-items: center;
+  height: 50px;
+`
+
+export const MethodParagraph = styled.p<IRequests>`
+  color: white;
+  background-color: ${(props) => methods[props.method]};
+  padding: 0.2rem;
+  border-radius: 0.3rem;
+  font-size: 1.0rem;
+  font-weight: 600;
+  width: 40px;
+  height: 30px;
+  text-align: center;
+  margin-right: 0.5rem;
+`
+export const TitleParagraph = styled.p`
+  font-weight: 600;
+`
+
+export const Url = styled(Pre)`
+  margin-top: -20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
--- components/Requests.tsx
+++ components/Requests.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import { type IRequests } from '../interfaces'
-import * as S from './Requests.style'
+import { CopyToClipboard } from 'nextra/components'
+import * as Styled from './Requests.style'
 
 export default function Requests (props: IRequests): JSX.Element {
   const { method, url, title } = props
+  const fullUrl = `https://api.spalla.io/${url as string}`
 
   return (
-  <section>
-    <div>{url} {title}</div>
-    <S.Method method={method}>{ method }</S.Method>
-  </section>
+  <Styled.Container>
+    <Styled.DivContainer>
+    <Styled.DivContainerHead>
+    <Styled.MethodParagraph method={ method }> { method } </Styled.MethodParagraph>
+    <Styled.TitleParagraph> { title } </Styled.TitleParagraph>
+    </Styled.DivContainerHead>
+    <Styled.Url>
+    { fullUrl }
+      <CopyToClipboard getValue={ () => fullUrl }/>
+    </Styled.Url>
+    </Styled.DivContainer>
+  </Styled.Container>
   )
 }
